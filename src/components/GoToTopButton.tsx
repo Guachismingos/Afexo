03d174133a1fe1b6ca3aea4cb8111d87d5d754d6
gradiel12 <gradiel101rojas@gmail.com>
2022-05-09 01:19:52 -0600

    Update GoToTopButton.tsx

--- src/components/GoToTopButton.tsx
+++ src/components/GoToTopButton.tsx
@@ -20,7 +20,7 @@ const GoToTopButton = () => {
       onClick={() => window.scrollTo(0, 0)}
       hidden={hidden}
       size="lg"
-      className="go-to-top-button rounded-circle bg-blue animate__animated animate__fadeInUp" 
+      className="go-to-top-button rounded-circle bg-blue animate__animated animate__fadeInUp"
     >
       <FontAwesomeIcon icon={faArrowCircleUp} />
     </Button>
